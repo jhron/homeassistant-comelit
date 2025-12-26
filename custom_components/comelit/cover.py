105371a62c2cf015a337110074c3bee790a2f183
jhron <jirka.hron@gmail.com>
2025-12-26 18:22:40 +0100

    Update cover integration: add device class, features, and improve position handling

--- custom_components/comelit/cover.py
+++ custom_components/comelit/cover.py
@@ -1,10 +1,14 @@
-"""Platform for light integration."""
+"""Platform for cover integration."""
 import logging
 
 from homeassistant.const import STATE_CLOSED, STATE_OPENING, STATE_CLOSING
 
 from .const import DOMAIN
-from homeassistant.components.cover import (CoverEntity)
+from homeassistant.components.cover import (
+    CoverEntity,
+    CoverDeviceClass,
+    CoverEntityFeature,
+)
 from .comelit_device import ComelitDevice
 
 
@@ -27,6 +31,20 @@ class ComelitCover(ComelitDevice, CoverEntity):
         else:
             self._position = None
 
+        # Set supported features based on position availability
+        self._attr_supported_features = (
+            CoverEntityFeature.OPEN
+            | CoverEntityFeature.CLOSE
+            | CoverEntityFeature.STOP
+        )
+        if position != -1:
+            self._attr_supported_features |= CoverEntityFeature.SET_POSITION
+
+    @property
+    def device_class(self) -> CoverDeviceClass:
+        """Return the device class of the cover."""
+        return CoverDeviceClass.SHUTTER
+
     @property
     def is_closed(self):
         return self._state == STATE_CLOSED
@@ -40,17 +58,22 @@ class ComelitCover(ComelitDevice, CoverEntity):
         return self._state == STATE_CLOSING
 
     @property
-    def current_cover_position(self):  # -> int | None:
+    def current_cover_position(self):
+        """Return current position of cover (0-100)."""
         if self._position is None:
             return None
         else:
             return 100 - self._position
-            
-    def set_cover_position(self, position, **kwargs):
-        _LOGGER.debug(f"Trying to SET POSITION {position} cover {self.name}! _state={self._state}")
-        self._hub.cover_position(self._id, 100 - position)
+
+    def set_cover_position(self, **kwargs):
+        """Set cover position."""
+        position = kwargs.get("position")
+        if position is not None:
+            _LOGGER.debug(f"Trying to SET POSITION {position} cover {self.name}! _state={self._state}")
+            self._hub.cover_position(self._id, 100 - position)
 
     def open_cover(self, stopping=False, **kwargs):
+        """Open the cover."""
         _LOGGER.debug(f"Trying to OPEN cover {self.name}! _state={self._state}")
         self._hub.cover_up(self._id)
         if not stopping:
@@ -58,13 +81,15 @@ class ComelitCover(ComelitDevice, CoverEntity):
         self.schedule_update_ha_state()
 
     def close_cover(self, stopping=False, **kwargs):
+        """Close the cover."""
         _LOGGER.debug(f"Trying to CLOSE cover {self.name}! _state={self._state}")
         self._hub.cover_down(self._id)
         if not stopping:
             self._state = STATE_CLOSING
         self.schedule_update_ha_state()
-    
+
     def update_state(self, state, position):
+        """Update cover state from hub."""
         super().update_state(state)
 
         if position != -1:
@@ -74,6 +99,7 @@ class ComelitCover(ComelitDevice, CoverEntity):
                 self.schedule_update_ha_state()
 
     def stop_cover(self, **kwargs):
+        """Stop the cover."""
         _LOGGER.debug(f"Trying to STOP cover {self.name}! is_opening={self.is_opening}, is_closing={self.is_closing}")
         if self.is_opening:
             self.close_cover(stopping=True)
